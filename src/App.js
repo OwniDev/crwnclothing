dce8a9c4addc88a09ab2326ad0be519caab62ba3
auxaneN <a.nitharum@sfr.fr>
2020-02-05 05:33:33 -0500

    Updated menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route, Link } from 'react-router-dom';
 // CSS STYLESHEETS
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Link to={`${props.match.url}/13`}></Link>
+      <h1> Hats Page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
