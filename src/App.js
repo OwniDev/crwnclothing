3b0ebf367540e61d177ac16ca5a968e5338c627f
auxaneN <a.nitharum@sfr.fr>
2020-02-06 04:47:16 -0500

    Added shop page, collection preview cmpnt and collection item cpmnt

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // CSS STYLESHEETS
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Link to={`${props.match.url}/13`}></Link>
-      <h1> Hats Page</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
